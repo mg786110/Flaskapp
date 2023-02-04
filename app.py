8dc40ed3f3ed8c866ffc05f20066b4657060609b
mg786110 <mgoani@gmail.com>
2023-02-04 18:31:34 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import json
+from flask import Flask , jsonify,request
+
+app = Flask(__name__)
+
+@app.route(rule = '/', methods = ['GET'])
+def main():
+    return "Hello world"
+
+    
+if __name__ == '__main__':
+    app.run(host ="0.0.0.0",port="8080",debug = True)
